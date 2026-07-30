a376ce62310884854bae7695315cf9e36754c1b2
gabrieldjou-hue <gabrieldjou-hue@users.noreply.github.com>
2026-07-30 11:34:53 -0300

    Initial commit - Tour360 V1

---
+++ data.js
@@ -0,0 +1,575 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salo-1",
+      "name": "Salão 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.081741100240059,
+        "pitch": 0.11492916833538303,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1120651994327373,
+          "pitch": 0.39908720606114123,
+          "rotation": 0,
+          "target": "1-salo-2"
+        },
+        {
+          "yaw": -2.0407814047226367,
+          "pitch": 0.17378862217034374,
+          "rotation": 0,
+          "target": "6-salo-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5938143888315306,
+          "pitch": 0.6579777194177634,
+          "title": "Entrada",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6130617572848802,
+          "pitch": 0.6909850522727545,
+          "title": "Entrada",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-salo-2",
+      "name": "salão 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.936474353314477,
+        "pitch": 0.04999945595133326,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.132813881140544,
+          "pitch": 0.3557382496049222,
+          "rotation": 0,
+          "target": "2-salo-3"
+        },
+        {
+          "yaw": 0.09350428660745358,
+          "pitch": 0.47973177700913183,
+          "rotation": 0,
+          "target": "0-salo-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-salo-3",
+      "name": "salão 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.9254103252685617,
+        "pitch": 0.27593072435178456,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1352416036567483,
+          "pitch": 0.27994057906501624,
+          "rotation": 0,
+          "target": "3-salo-4"
+        },
+        {
+          "yaw": 0.0342879145771704,
+          "pitch": 0.6718557463335841,
+          "rotation": 0,
+          "target": "1-salo-2"
+        },
+        {
+          "yaw": -1.2932142361678505,
+          "pitch": 0.21830783977289414,
+          "rotation": 0,
+          "target": "6-salo-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salo-4",
+      "name": "salão 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.478133682238152,
+        "pitch": 0.163536424366427,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8399711925472122,
+          "pitch": 0.24193711755794567,
+          "rotation": 0,
+          "target": "8-salo-housemix"
+        },
+        {
+          "yaw": 1.511196353152048,
+          "pitch": 0.46015804425048046,
+          "rotation": 0,
+          "target": "2-salo-3"
+        },
+        {
+          "yaw": 3.0070900325803365,
+          "pitch": 0.4776294414555906,
+          "rotation": 0,
+          "target": "4-salo-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24033549734386028,
+          "pitch": 0.8002397744384417,
+          "title": "Palco 6mx2,5m",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.42869372318691035,
+          "pitch": 0.3852727964347906,
+          "title": "4 Poltronas",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6664979266897859,
+          "pitch": 0.08033350896831593,
+          "title": "Vídeo Wall de 110\"",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.11062459733057395,
+          "pitch": 0.0788321493115145,
+          "title": "Banner removível",
+          "text": "Possibilita reenvelopamento da estrutura com arte do evento"
+        },
+        {
+          "yaw": 3.00629908314235,
+          "pitch": -0.04622694599119015,
+          "title": "Total de Lugares",
+          "text": "150 cadeiras com possibilidade de 200 lugares total, no salão."
+        },
+        {
+          "yaw": 0.8813417000313208,
+          "pitch": 0.16270127801422518,
+          "title": "Cofee-Break",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.0880902985206973,
+          "pitch": 0.19662950622514686,
+          "title": "House Mix",
+          "text": "2 Mesas 1,40m"
+        }
+      ]
+    },
+    {
+      "id": "4-salo-5",
+      "name": "salão 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.1103902045417424,
+        "pitch": 0.025513237674656963,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0370040530053544,
+          "pitch": 0.08979102627804103,
+          "rotation": 0,
+          "target": "5-salo-6"
+        },
+        {
+          "yaw": 0.5013624837259112,
+          "pitch": 0.42010442823199057,
+          "rotation": 0,
+          "target": "2-salo-3"
+        },
+        {
+          "yaw": -0.9806477367754827,
+          "pitch": 0.32518410910654616,
+          "rotation": 0,
+          "target": "8-salo-housemix"
+        },
+        {
+          "yaw": 0.949236849452074,
+          "pitch": 0.28025968902287524,
+          "rotation": 0,
+          "target": "1-salo-2"
+        },
+        {
+          "yaw": 1.277116395055664,
+          "pitch": 0.13005813753421158,
+          "rotation": 0,
+          "target": "0-salo-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-salo-6",
+      "name": "salão 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.0411764500504095,
+        "pitch": -0.04771030435029999,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0308668494763875,
+          "pitch": 0.11294842567579266,
+          "rotation": 0,
+          "target": "7-salo-copa-1"
+        },
+        {
+          "yaw": 0.025532148301023128,
+          "pitch": 0.3215157567031923,
+          "rotation": 0,
+          "target": "4-salo-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-salo-9",
+      "name": "salão 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.031018355527797,
+        "pitch": 0.16166923744466288,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.073518136521873,
+          "pitch": 0.17534411504276726,
+          "rotation": 0,
+          "target": "0-salo-1"
+        },
+        {
+          "yaw": -1.5610596143865667,
+          "pitch": 0.235085064175518,
+          "rotation": 0,
+          "target": "1-salo-2"
+        },
+        {
+          "yaw": -1.0678281339075184,
+          "pitch": 0.21742417898732747,
+          "rotation": 0,
+          "target": "2-salo-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.861545099406313,
+          "pitch": 0.099729288547028,
+          "title": "Área Coffee Breack",
+          "text": "2 mesas<div>4 aparadores</div>"
+        },
+        {
+          "yaw": -2.8927853018237553,
+          "pitch": -0.010620369298873555,
+          "title": "2 Banheiros",
+          "text": "Todos os banheiros são acessíveis"
+        },
+        {
+          "yaw": -1.7588357452373184,
+          "pitch": 0.02533487346875596,
+          "title": "3 Banheiros",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-salo-copa-1",
+      "name": "salão copa 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.2270818414740887,
+        "pitch": 0.09736203625062068,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6586298766587273,
+          "pitch": 0.1553820422312029,
+          "rotation": 0,
+          "target": "9-salo-copa-2"
+        },
+        {
+          "yaw": 0.18445727504788323,
+          "pitch": 0.2079963191872345,
+          "rotation": 0,
+          "target": "5-salo-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-salo-housemix",
+      "name": "salão housemix",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.0868738583296125,
+        "pitch": 0.4042050135025903,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.129490551565002,
+          "pitch": 0.27902751313702545,
+          "rotation": 0,
+          "target": "3-salo-4"
+        },
+        {
+          "yaw": -2.3565205119861243,
+          "pitch": 0.16549227187876525,
+          "rotation": 0,
+          "target": "4-salo-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.386326708898028,
+          "pitch": 0.36162874938816003,
+          "title": "Microfones SF Sennheiser EWD",
+          "text": "4 Microfones Bastãp"
+        },
+        {
+          "yaw": 2.1795278597142573,
+          "pitch": 0.4651447341555741,
+          "title": "Console de áudio Behringer Xr-18",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.376665825707139,
+          "pitch": 0.5772318886404211,
+          "title": "Sistema streaming com vMix + Atem Mini",
+          "text": "Suporte para 4 entradas diversa"
+        }
+      ]
+    },
+    {
+      "id": "9-salo-copa-2",
+      "name": "salão copa 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.026481554366775,
+        "pitch": 0.30132823800190955,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.39305874329396673,
+          "pitch": 0.41683314069252475,
+          "rotation": 0,
+          "target": "7-salo-copa-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
